f7a3acb973132c25a2c276165ffa421977eeac44
Calculadora_RobertoCuascota <rccuascotap@utn.edu.ec>
2024-04-28 22:22:01 -0500

    Port

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def predict():
     return jsonify({'predicted_number': int(predicted_number)})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run(host="0.0.0.0", port=8080,debug=True)
