0a046616ca765a4869df27ff9740d3ae125cb1b3
Calculadora_RobertoCuascota <rccuascotap@utn.edu.ec>
2024-04-28 22:15:20 -0500

    appp 1

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def predict():
     return jsonify({'predicted_number': int(predicted_number)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000,debug=True)
